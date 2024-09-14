cf289591cb2e5042fba93933d741dddf1971122d
Remy-Oliveira <remy.musico@gmail.com>
2024-09-14 17:40:56 -0300

    condicionais

---
+++ javascript/condicionais.js
@@ -0,0 +1,7 @@
+var velocidade = 6
+console.log(`velocidade do seu carro é ${velocidade}Km/h`) // console.log é o print no console
+if (velocidade > 60) { // Estrutura condicional simples
+    console.log('Ultrapassou o liomite da velocidade. MULTADO! ')
+} else {
+    console.log(`Dirija sempre usando o cinto de segurança!`)
+}
