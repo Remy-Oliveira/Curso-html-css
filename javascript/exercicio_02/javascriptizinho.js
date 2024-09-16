273ca49975af02b66e46173ebd4ea55671a3ecf1
Remy-Oliveira <remy.musico@gmail.com>
2024-09-16 02:37:32 -0300

    comentei o exercicio 2

--- javascript/exercicio_02/javascriptizinho.js
+++ javascript/exercicio_02/javascriptizinho.js
@@ -1,38 +1,37 @@
-function verificar() {
-    var data = new Date()
-    var ano = data.getFullYear()
-    var form_ano = document.querySelector('#txtano')
-    var resultado = document.querySelector('#resultado p')
-    if (form_ano.value.length == 0 || form_ano.value > ano) {
-        alert('Verifique os dados e tente novamente')
+function verificar() {  // função que foi disparada do botão
+    var ano = new Date().getFullYear() // pega o ano atual
+    var form_ano = document.querySelector('#txtano') // pega o input da data de nascimento
+    var resultado = document.querySelector('#resultado p') // pega o paragrafo da div
+    if (Number(form_ano.value) > ano) { // se ele nasceu do futuro! kkk
+        alert('Verifique os dados e tente novamente') // alerta de erro
     } else {
-        var sexo = document.getElementsByName('radiosex')
-        var idade = ano - Number(form_ano.value)
+        var sexo = document.getElementsByName('radiosex') // pegando os dois checkboxes
+        var idade = ano - Number(form_ano.value) // idade é a data de nascimento pela  o ano atual. Number converte o txt para numero
         //resultado.innerHTML = `Idade calculada: ${idade}`
-        var genero = ' '
-        var img = document.createElement('img')
-        img.setAttribute('id', 'foto')
-        if (sexo[0].checked) {
-            genero = 'Homem'
-            if (idade >= 0 && idade < 10) {
+        var genero = ' ' // declaração do genero vazio
+        var img = document.createElement('img') // crio uma imagem...
+        img.setAttribute('id', 'foto') // ... com o id = foto
+        if (sexo[0].checked) { // se o radio de numero 01 é masculino...
+            genero = 'Homem'  // genero é masculino
+            if (idade >= 0 && idade < 10) { // se a idade for maior ou igual a 0 e tambem menor que 10... é uma criançinha...
                 //crianca
-                img.setAttribute('src', 'imagens/menino.jpg')
-                document.body.style.background = 'linear-gradient(to top, #995f1f, #89604e)'
-            } else if (idade >= 10 && idade < 21) {
+                img.setAttribute('src', 'imagens/menino.jpg') // coloco a imagem de masculina de uma criança...
+                document.body.style.background = 'linear-gradient(to top, #995f1f, #89604e)' // gradiente bonitão...
+            } else if (idade >= 10 && idade < 21) { // se a idade for maior que 10 e tambem menor que 21, é um xovemzinho...
                 //xovem
-                img.setAttribute('src', 'imagens/garoto.jpg')
-                document.body.style.background = 'linear-gradient(to top, #cd2b3a, #0c091a)'
-            } else if (idade < 50) {
+                img.setAttribute('src', 'imagens/garoto.jpg') // imagem de um jovenzinho...
+                document.body.style.background = 'linear-gradient(to top, #cd2b3a, #0c091a)' // o gradiente bobnitão...
+            } else if (idade < 50) { // menor que 50 é um adulto masculino
                 // adulto
-                img.setAttribute('src', 'imagens/homem.jpg')
-                document.body.style.background = 'linear-gradient(to top, #d02e3d, #d0902c)'
-            } else if (idade > 50) {
+                img.setAttribute('src', 'imagens/homem.jpg') // foto do adulto masculino...
+                document.body.style.background = 'linear-gradient(to top, #d02e3d, #d0902c)' // já sabe, neh?
+            } else if (idade > 50) { // se for maior que 50, é um mestre yoda!
                 //veio
-                img.setAttribute('src', 'imagens/velho.jpg')
-                document.body.style.background = 'linear-gradient(to top, #c18062, #29556e)'
+                img.setAttribute('src', 'imagens/velho.jpg') // ancião...
+                document.body.style.background = 'linear-gradient(to top, #c18062, #29556e)' // gradiente
             }
-        } else if (sexo[1].checked) {
-            genero = 'Mulher'
+        } else { // se for mulher...
+            genero = 'Mulher' // genero mulher
             if (idade >= 0 && idade < 10) {
                 //crianca
                 img.setAttribute('src', 'imagens/menina.jpg')
@@ -51,8 +50,8 @@ function verificar() {
                 document.body.style.background = 'linear-gradient(to top, #1a1917, #aa8b48)'
             }
         }
-        resultado.style.textAlign = 'center'
-        resultado.innerHTML = `Detectamos um(a) <strong>${genero}</strong> com <strong>${idade}</strong> ano(s).`
+        resultado.style.textAlign = 'center' // alinhar texto 
+        resultado.innerHTML = `Detectamos um(a) <strong>${genero}</strong> com <strong>${idade}</strong> ano // altera o texto da div por essa interpolação de string!(s).`
         resultado.appendChild(img)
     }
 }
