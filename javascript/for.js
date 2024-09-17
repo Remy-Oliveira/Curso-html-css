cd2496d895cd9f88d8331f4bfaedaba09bea1c9c
Remy-Oliveira <remy.musico@gmail.com>
2024-09-17 16:18:48 -0300

    uso do for

---
+++ javascript/for.js
@@ -0,0 +1,4 @@
+// for -> declara a variável, condição, increment0
+for (var contador = 0; contador <= 10; contador++) {
+    console.log(`Contador = ${contador}`)
+}
---
+++ while.js
@@ -0,0 +1,20 @@
+//console.log('Tudo Bem!')
+
+// Do => Executa antes da condição
+// While => Executa depois da condição
+/*
+
+var contador = 1
+do {
+    contador++
+    console.log('l200')
+} while (contador <= 5)
+*/
+
+/*
+var contador = 1
+while (contador <= 5) {
+    console.log(`Contador = ${contador}`)
+    contador++ // contador = contador + 1
+}
+*/
