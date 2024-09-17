4e6be4501313e2c46a319a884f04028616a44f66
Remy-Oliveira <remy.musico@gmail.com>
2024-09-17 19:48:47 -0300

    exercicio 03

--- javascript/exercicio_03/js_03.js
+++ javascript/exercicio_03/js_03.js
@@ -1,11 +1,17 @@
 function chama_java() {
-    var contador = document.getElementById('inicio').value
-    var fim = document.getElementById('fim').value
-    var passo = document.getElementById('passo').value
+    //parseInt(document.getElementById('inicio').value);
+    var inicio = Number(document.getElementById('inicio').value)
+    var fim = Number(document.getElementById('fim').value)
+    var passo = Number(document.getElementById('passo').value)
     var mostra_tela = document.getElementById('p_02')
-    
-    for (contador; contador <= fim; contador = passo) {
-        mostra_tela.innerText = `Remy: ${fim}`
+    mostra_tela.innerText = ''
+    if (inicio == 0 || fim == 0 || passo == 0) {
+        alert('combinação inválida')
+        return
+    } else {
+        for (inicio; inicio <= fim; inicio += passo) {
+            mostra_tela.innerText += `${inicio} ❌ `
+        }
     }
-    mostra_tela.innerText = `Remy: ${fim}`
+    mostra_tela.innerText += `Fim `
 }
