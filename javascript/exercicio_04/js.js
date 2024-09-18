ab48d8f6beacef4d446f8c81464d304c1307563b
Remy-Oliveira <remy.musico@gmail.com>
2024-09-18 01:58:44 -0300

    ex_04

---
+++ javascript/exercicio_04/js.js
@@ -0,0 +1,15 @@
+function chama_java() {
+    var multiplicando = Number(document.getElementById('entrada').value)
+    var resultado = document.getElementById('resultado_select');
+    resultado.innerHTML = '';
+    if (multiplicando == 0 ) {
+        alert('Digite um valor válido')
+        return
+    } else {
+        for (var contador = 1; contador <= 10; contador++) {
+            var saida = document.createElement('option')
+            saida.text = `${multiplicando} x ${contador} = ` + multiplicando * contador
+            resultado.appendChild(saida);
+        }
+    }
+}
