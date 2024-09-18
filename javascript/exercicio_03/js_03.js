dc48276ffdb8a7ebd6cb93696cda2e1c601ed2f9
Remy-Oliveira <remy.musico@gmail.com>
2024-09-18 00:34:13 -0300

    update

--- javascript/exercicio_03/js_03.js
+++ javascript/exercicio_03/js_03.js
@@ -4,14 +4,18 @@ function chama_java() {
     var fim = Number(document.getElementById('fim').value)
     var passo = Number(document.getElementById('passo').value)
     var mostra_tela = document.getElementById('p_02')
-    mostra_tela.innerText = ''
+    mostra_tela.innerText = ' '
     if (inicio == 0 || fim == 0 || passo == 0) {
         alert('combinação inválida')
         return
-    } else {
+    } else if (inicio < fim) {
         for (inicio; inicio <= fim; inicio += passo) {
             mostra_tela.innerText += `${inicio} ❌ `
         }
+    } else {
+        for (inicio; inicio >= fim; inicio -= passo) {
+            mostra_tela.innerText += `${inicio} ❌ `
+        }
     }
-    mostra_tela.innerText += `Fim `
+    mostra_tela.innerText += `Fim`
 }
